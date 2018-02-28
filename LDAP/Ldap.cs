ef62b3516fa21abc02e7daed62335e4f035c0ad5
Casper Andersen <zbccaand49@zbc.dk>
2018-02-28 12:45:16 +0100

    FIRSt
    FIRST

---
+++ LDAP/Ldap.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.DirectoryServices.Protocols;
+using System.Diagnostics;
+
+namespace LDAP
+{
+    class Ldap
+    {
+        private NetworkCredential credentials;
+        private LdapDirectoryIdentifier serverId;
+        private LdapConnection connection;
+        //TESTCOMMENT
+        private SearchResponse response;
+        private SearchRequest request;
+        private AddRequest addrequest;
+        private DeleteRequest delrequest;
+
+        public void Init()
+        {
+            credentials = new NetworkCredential(@"Administrator", "Team Soccer1", "Team-Soccer.local");
+            serverId = new LdapDirectoryIdentifier("192.168.0.1", 389);
+        }
+
+        public bool Connect()
+        {
+            try
+            {
+                connection = new LdapConnection(serverId, credentials);
+                connection.Bind();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool AddUser(string username, string passwd)
+        {
+            try
+            {
+                addrequest = new AddRequest("cn=" + username + ",CN=Users,dc=Team-Soccer,dc=Local", new DirectoryAttribute[] {
+            new DirectoryAttribute("cn", username),
+            new DirectoryAttribute("ou", "users"),
+            new DirectoryAttribute("userPassword", passwd),
+            new DirectoryAttribute("objectClass", new string[] {"top","person", "organizationalPerson", "user" })
+            });
+                this.connection.SendRequest(addrequest);
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public bool DeleteUser(string username, string userdn)
+        {
+            if (username != "Administrator") {
+                try
+                {
+                    delrequest = new DeleteRequest(userdn);
+                    this.connection.SendRequest(delrequest);
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                    return false;
+                }
+        }
+            return true;
+        }
+        public SearchResponse SendSearchRequest(string path, string filter)
+        {
+            try
+            {
+                Debug.WriteLine("Making request");
+                request = new SearchRequest(path+",dc=team-soccer,dc=local", "(objectClass="+filter+")", System.DirectoryServices.Protocols.SearchScope.Subtree, null);
+                Debug.WriteLine("Getting Response");
+                response = (SearchResponse)connection.SendRequest(request);
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("error at testresponse");
+            }
+            return response;
+        }
+
+        public SearchResponse TestRequest()
+        {
+            try
+            {
+                Debug.WriteLine("Making request");
+                request = new SearchRequest("cn=users,dc=team-soccer,dc=local", "(objectClass=user)", System.DirectoryServices.Protocols.SearchScope.Subtree, null);
+                Debug.WriteLine("Getting Response");
+                response = (SearchResponse)connection.SendRequest(request);
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("error at testresponse");
+            }
+            return response;
+        }
+
+        public void Clean()
+        {
+            connection.Dispose();
+        }
+
+    }
+}
---
+++ LDAP/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices.Protocols;
+
+namespace LDAP
+{
+    class Program
+    {
+        //Statics();
+        static string programname = "LDAP TO TEAMSOCCER";
+        static string[] menu =
+            {
+                    "Close",
+                    "Search AD",
+                    "Add user",
+                    "move user",
+                    "delete user"
+                };
+
+        static void Main(string[] args)
+        {
+            //INIT();
+            Ldap ldap = new Ldap();
+            ldap.Init();
+
+            string searchDir;
+            string searchFilter;
+            string user;
+            string pass;
+            string userdn;
+            ldap.Connect();
+
+            string menuint;
+
+            //PROGRAM();
+            Output(programname);
+            bool program = true;
+            while (program)
+            {
+                menuint = Menu(menu);
+                switch (menuint)
+                {
+                    case "0":
+                        program = false;
+                        break;
+                    case "1":
+                        searchDir = Input("Please input search Directory: ");
+                        searchFilter = Input("Please input filter:");
+                        PrintResponse(ldap.SendSearchRequest(searchDir, searchFilter));
+                        break;
+                    case "2":
+                        user = Input("Please Input username: ");
+                        pass = Input("Please input password: ");
+                        if (ldap.AddUser(user, pass)) {
+                            Output("User added");
+                        }
+                        else { Output("error\nPlease try again"); }
+                        break;
+                    case "4":
+                        user = Input("please Input username to delete: ");
+                        userdn = Input("please input the the full DistinguisedName of " + user + ": ");
+                        if (ldap.DeleteUser(user, userdn))
+                        {
+                            Output("User deleted");
+                        } else {Output("error\nPlease try again"); }
+                        break;
+                    case "-":
+                        PrintResponse(ldap.TestRequest());
+                        break;
+                    default:
+                        Output("Something went wrong");
+                        break;
+                }
+                Output("press anykey to return to the menu...");
+
+                Wait();
+                Clear();
+            }
+
+        }
+
+        //GUI METHODs
+        static void Output(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        static string Input(string text)
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+
+        static void Wait()
+        {
+            Console.ReadKey();
+        }
+
+        static void Clear()
+        {
+            Console.Clear();
+        }
+
+        static string Menu(string[] menu)
+        {
+            Output("\nMenu:");
+            for (int i = 1; i < menu.Length; i++)
+            {
+                Output(i + ". " + menu[i]);
+            }
+            Output("0. " + menu[0]);
+            Output("-. " + "VIEW ALL USERS");
+            return Input("\nEnter your choice: ");
+        }
+
+        //LOGIC METHODs
+        static void PrintResponse(SearchResponse response)
+        {
+            foreach(SearchResultEntry entry in response.Entries)
+            {
+                Output(entry.DistinguishedName);
+            }
+        }
+
+
+    }
+}
