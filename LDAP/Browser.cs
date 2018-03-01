9e22a2856978b3ba59b577941d9431bf98f0e732
zbcisand3 <36920966+zbcisand3@users.noreply.github.com>
2018-03-01 08:59:02 +0100

    Started on Forms

---
+++ LDAP/Browser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LDAP
+{
+    public partial class Browser : Form
+    {
+        private Connection connection;
+
+        public Browser(Connection connection)
+        {
+            this.connection = connection;
+            InitializeComponent();
+
+            GetView();
+        }
+
+        private void GetView()
+        {
+            
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LDAP/Connection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.DirectoryServices.Protocols;
+using System.Diagnostics;
+
+namespace LDAP
+{
+    public class Connection
+    {
+        private NetworkCredential credentials;
+        private LdapDirectoryIdentifier serverId;
+        private LdapConnection ldapConnection;
+
+        public NetworkCredential Credentials { get => credentials; }
+        public LdapDirectoryIdentifier Server { get => serverId; }
+
+        public Connection(string domainOrIP, string username, string password)
+        {
+            credentials = new NetworkCredential(username, password, domainOrIP);
+            serverId = new LdapDirectoryIdentifier(domainOrIP, 389);
+            ldapConnection = new LdapConnection(serverId, credentials);
+            ldapConnection.Bind();
+        }
+
+        public void SendRequest()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            ldapConnection.Dispose();
+        }
+    }
+}
